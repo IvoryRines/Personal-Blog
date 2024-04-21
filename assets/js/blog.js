e6743ad2cc0abc8f5cb4e74c845d03e2de42edf0
EyeVorE <subplot_05tracer@icloud.com>
2024-04-21 15:18:33 -0500

    Moved light dark toggle to logic.js and added scripts to both html to maintain functionality.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,17 +1,4 @@
-const backButton = getElementById('back-button');
 
-function toggleMode() {
-    const icon = document.getElementById('mode-icon');
-    const currentMode = document.body.classList.contains('dark-mode');
-  
-    if (currentMode) {
-      document.body.classList.remove('dark-mode');
-      icon.innerHTML = '🌘'; // Moon emoji for light mode
-    } else {
-      document.body.classList.add('dark-mode');
-      icon.innerHTML = '☀️'; // Sun emoji for dark mode
-    }
-  }
 
 // function displayBlogPosts(event) {
 //     event.preventDefault()
@@ -19,4 +6,6 @@ function toggleMode() {
 
 //     let newPost
 // }
+
+
 
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,15 +1,15 @@
-function toggleMode() {
-  const icon = document.getElementById('mode-icon');
-  const currentMode = document.body.classList.contains('dark-mode');
+// function toggleMode() {
+//   const icon = document.getElementById('mode-icon');
+//   const currentMode = document.body.classList.contains('dark-mode');
 
-  if (currentMode) {
-    document.body.classList.remove('dark-mode');
-    icon.innerHTML = '🌘'; // Moon emoji for light mode
-  } else {
-    document.body.classList.add('dark-mode');
-    icon.innerHTML = '☀️'; // Sun emoji for dark mode
-  }
-}
+//   if (currentMode) {
+//     document.body.classList.remove('dark-mode');
+//     icon.innerHTML = '🌘'; // Moon emoji for light mode
+//   } else {
+//     document.body.classList.add('dark-mode');
+//     icon.innerHTML = '☀️'; // Sun emoji for dark mode
+//   }
+// }
 
 
 
